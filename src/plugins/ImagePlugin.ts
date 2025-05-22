3ba9361a7382c72bdcee99c4f432aa6369c021ed
Sharuru <mave@foxmail.com>
2025-05-22 10:17:16 +0800

    Modify direct prompt keyword

--- src/plugins/ImagePlugin.ts
+++ src/plugins/ImagePlugin.ts
@@ -10,11 +10,12 @@ type ImagePluginArgs = {
 }
 
 export class ImagePlugin extends PluginBase<ImagePluginArgs> {
-    private readonly GPT_INSTRUCTIONS = "你是一位 AI 的 prompt 工程师，帮助用户为图像AI DALL-E创建优质提示。" + 
+    private readonly GPT_INSTRUCTIONS = "你是一位 AI 的 prompt 工程师，帮助用户创建优质的 AI 图片生成提示。" + 
     "用户会提供简短的图像描述，你需要将其转化为合适的提示文本。" + 
     "创建提示时，首先描述图像的外观和结构。其次，描述摄影风格，如相机角度、相机位置、镜头等。第三，描述光线和特定颜色。" + 
     "你的提示必须专注于整体图像，而不是描述其中的细节。" + 
     "如果用户没有提供，考虑添加一些流行词，例如'细节丰富'、'超细节'、'非常逼真'、'素描风格'、'街头艺术'、'绘画'等类似词语。" + 
+    "用户可能更偏爱日本动漫、卡通的风格的图片。" +
     "保持提示尽可能简单，且不超过400个字符。你只能回答生成的提示，不提供任何描述或解释。" +
     "并记住一定要使用与收到的描述相同的语种。比如用户的描述是中文，那么你的提示也应该是中文。" +
     "如果你无法分辨或不确信用户的描述语种，那默认使用简体中文。"
@@ -37,9 +38,9 @@ export class ImagePlugin extends PluginBase<ImagePluginArgs> {
         try {
             let imagePrompt;
             const msgText = msgData.post.message;
-            if(msgText.includes("【无需帮助】")) {
-                imagePrompt = msgText.split("【无需帮助】")[1];
-            } else {
+            if(msgText.startsWith("[直接生成图片]") || msgText.includes("[直接生成图片]")) {
+                imagePrompt = msgText.split("[直接生成图片]")[1].trim();
+            }else {
                 imagePrompt = await this.createImagePrompt(args.imageDescription)
             }
             if(imagePrompt) {
